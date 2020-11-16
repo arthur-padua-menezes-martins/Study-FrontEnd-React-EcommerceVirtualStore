8634d0e3999e5db07166abcb3f08ffc8fe6ae47a
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-16 09:48:32 -0300

    chore: add jest scripts

---
+++ src/domain/usecases/authentication/authentication.ts
@@ -0,0 +1,27 @@
+import {
+  IAccountModel
+} from '../../model/account/account'
+
+interface IAuthenticationParams {
+  [field: string]: string | object | boolean
+  personal: {
+    [field: string]: string
+    name: string
+    email: string
+    password: string
+    passwordConfirmation: string
+  }
+  address: {
+    [field: string]: string
+    cep: string
+    street: string
+    number: string
+    neighborhood: string
+    city: string
+    state: string
+  }
+}
+
+export interface IAuthentication {
+  auth (params: IAuthenticationParams): Promise<IAccountModel>
+}
